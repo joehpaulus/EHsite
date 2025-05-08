f298900f3cb94d3775484b8a92393313376a3bd1
joehpaulus <joho8439@colorado.edu>
2025-05-08 13:17:06 -0600

    Fix Tailwind config and clean up for GitHub Pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'italian-green': '#009246',
+        'italian-white': '#FFFFFF',
+        'italian-red': '#CE2B37',
+      },
+    },
+  },
+  plugins: [],
+} 
